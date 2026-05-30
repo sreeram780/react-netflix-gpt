22abdf23ed5c41ab5dabf0e2455174e355aec03f
sreeram780 <sreenu.ram2@gmail.com>
2026-05-30 21:06:38 +0530

    - updated Header UI

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,6 +6,7 @@ const Header = () => {
         src="https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production_2026-05-14/consent/87b6a5c0-0104-4e96-a291-092c11350111/019ae4b5-d8fb-7693-90ba-7a61d24a8837/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
         alt="logo"
       />
+      <button className="py-2 px-4 mx-4 my-2 bg-purple-800 text-white rounded-lg">GPT Search</button>
     </div>
   );
 };
