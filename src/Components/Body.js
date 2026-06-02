2c6f08b9add27f03c6d591de539c86e817e0e1f4
sreeram780 <sreenu.ram2@gmail.com>
2026-06-02 19:17:34 +0530

    - updated body and gpt search component

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,30 +1,28 @@
-import { createBrowserRouter } from "react-router-dom"
-import Browse from "./Browse"
-import Login from "./Login"
-import { RouterProvider } from "react-router-dom"
-import { Provider } from "react-redux"
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Browse from "./Browse";
+import Login from "./Login";
+import { Provider } from "react-redux";
+import appStore from "../utils/appStore";
 
 const Body = () => {
-
-const appRouter = createBrowserRouter([
+  const appRouter = createBrowserRouter([
     {
-        path:"/",
-        element: <Login />
+      path: "/",
+      element: <Login />,
     },
     {
-        path:"/Browse",
-        element:<Browse />
-    }
+      path: "/browse",
+      element: <Browse />,
+    },
+  ]);
 
-])
+  return (
+    <div>
+      <Provider store={appStore}>
+        <RouterProvider router={appRouter} />
+      </Provider>
+    </div>
+  );
+};
 
-
-    return (<div>
-        <Provider >
-            <RouterProvider router={appRouter} />
-        </Provider>
-    </div>)
-}
-
-export default Body
+export default Body;
--- src/Components/GPTSearch.js
+++ src/Components/GPTSearch.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useRef } from "react";
+import { BACKGROUND_IMG_URL } from "../utils/constant";
 
 const GPTSearch = () => {
-  return (
-    <div>GPTSearch</div>
-  )
-}
+  const searchText = useRef(null);
 
-export default GPTSearch
+  const handleGPTSearchClick = () => {
+    // TODO: Call OpenAI GPT API with searchText.current.value
+    // Then use the GPT response to search TMDB for each movie suggestion
+    console.log("GPT Search:", searchText.current.value);
+  };
+
+  return (
+    <div>
+      <div className="absolute -z-10">
+        <img
+          className="h-screen object-cover w-screen"
+          src={BACKGROUND_IMG_URL}
+          alt="background"
+        />
+      </div>
+      <div className="pt-[35%] flex justify-center">
+        <form
+          className="w-1/2 bg-black grid grid-cols-12 rounded-lg overflow-hidden"
+          onSubmit={(e) => e.preventDefault()}
+        >
+          <input
+            ref={searchText}
+            type="text"
+            className="p-4 m-4 col-span-9 bg-gray-800 text-white rounded-lg outline-none"
+            placeholder="What would you like to watch?"
+          />
+          <button
+            className="col-span-3 m-4 py-2 px-4 bg-red-700 text-white rounded-lg font-semibold"
+            onClick={handleGPTSearchClick}
+          >
+            Search
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default GPTSearch;
