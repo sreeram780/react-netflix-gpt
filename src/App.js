efdd5cd5253322c1a9995a7b3f79a46c27204ce6
sreeram780 <sreenu.ram2@gmail.com>
2026-06-03 21:32:15 +0530

    - updated app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import "./index.css";
+import { Provider } from "react-redux";
 import Body from "./Components/Body";
+import appStore from "./utils/appStore";
 
 function App() {
   return (
-   <Body />
-   );
+    <Provider store={appStore}>
+      <Body />
+    </Provider>
+  );
 }
 
 export default App;
