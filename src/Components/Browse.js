828942de69c255739a83be6d146956deff46d60e
sreeram780 <sreenu.ram2@gmail.com>
2026-06-04 17:20:33 +0530

    - updated browse component

--- src/Components/Browse.js
+++ src/Components/Browse.js
@@ -1,13 +1,29 @@
-import Header from "./Header"
-import MainContainer from "./MainContainer"
-import SecondaryContainer from "./SecondaryContainer"
+import Header from "./Header";
+import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
+import MainContainer from "./MainContainer";
+import SecondaryContainer from "./SecondaryContainer";
+import usePopularMovies from "../hooks/usePopularMovies";
+import GptSearch from "./GptSearch";
+import { useSelector } from "react-redux";
 
 const Browse = () => {
-    return <div>
-        <Header />
-        <MainContainer />
-        <SecondaryContainer />
-    </div>
-}
+  const showGptSearch = useSelector((store) => store.gpt.showGptSearch);
 
-export default Browse
+  useNowPlayingMovies();
+  usePopularMovies();
+
+  return (
+    <div>
+      <Header />
+      {showGptSearch ? (
+        <GptSearch />
+      ) : (
+        <>
+          <MainContainer />
+          <SecondaryContainer />
+        </>
+      )}
+    </div>
+  );
+};
+export default Browse;
