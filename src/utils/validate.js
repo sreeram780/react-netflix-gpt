8792c626565a9f98bfab13b190ad629cc6d21dd0
sreeram780 <sreenu.ram2@gmail.com>
2026-06-06 19:48:55 +0530

    - updated validation of email and password

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,10 +1,12 @@
 export const checkValidData = (email, password) => {
-    const isEmailValid = /^(^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$)/.test(email);
-    const isPasswordValid = /^(^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$)/.test(password);
+  const isEmailValid = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/.test(
+    email
+  );
+  const isPasswordValid =
+    /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
 
-    if (!isEmailValid) return "Email ID is Not valid";
-    if (!isPasswordValid) return "Password is Not valid";
+  if (!isEmailValid) return "Email ID is not valid";
+  if (!isPasswordValid) return "Password is not valid";
 
-    return null
-
-}
+  return null;
+};
