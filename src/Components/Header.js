5c1ce360d4a0ded7f6c96880354a36a62999aed7
sreeram780 <sreenu.ram2@gmail.com>
2026-06-07 21:52:58 +0530

    - updated header component

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,47 +1,92 @@
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { LOGO_URL } from "../utils/constant";
-import { removeUser } from "../utils/userSlice";
+import { LOGO, SUPPORTED_LANGUAGES } from "../utils/constants";
 import { auth } from "../utils/firebase";
-import { signOut } from "firebase/auth";
+import { addUser, removeUser } from "../utils/userSlice";
+import { toggleGptSearchView } from "../utils/gptSlice";
+import { changeLanguage } from "../utils/configSlice";
 
 const Header = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const user = useSelector((store) => store.user);
-
+  const showGptSearch = useSelector((store) => store.gpt.showGptSearch);
   const handleSignOut = () => {
     signOut(auth)
-      .then(() => {
+      .then(() => {})
+      .catch((error) => {
+        navigate("/error");
+      });
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const { uid, email, displayName, photoURL } = user;
+        dispatch(
+          addUser({
+            uid: uid,
+            email: email,
+            displayName: displayName,
+            photoURL: photoURL,
+          })
+        );
+        navigate("/browse");
+      } else {
         dispatch(removeUser());
         navigate("/");
-      })
-      .catch((error) => console.error(error));
+      }
+    });
+
+    // Unsiubscribe when component unmounts
+    return () => unsubscribe();
+  }, []);
+
+  const handleGptSearchClick = () => {
+    // Toggle GPT Search
+    dispatch(toggleGptSearchView());
+  };
+
+  const handleLanguageChange = (e) => {
+    dispatch(changeLanguage(e.target.value));
   };
 
   return (
-    <div className="absolute w-full px-8 py-2 bg-gradient-to-b from-black z-20 flex justify-between items-center">
-      <img className="w-44" src={LOGO_URL} alt="Netflix logo" />
+    <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex flex-col md:flex-row justify-between">
+      <img className="w-44 mx-auto md:mx-0" src={LOGO} alt="logo" />
       {user && (
-        <div className="flex items-center gap-4">
-          <button className="py-2 px-4 bg-purple-800 text-white rounded-lg hover:bg-purple-700">
-            GPT Search
+        <div className="flex p-2 justify-between">
+          {showGptSearch && (
+            <select
+              className="p-2 m-2 bg-gray-900 text-white"
+              onChange={handleLanguageChange}
+            >
+              {SUPPORTED_LANGUAGES.map((lang) => (
+                <option key={lang.identifier} value={lang.identifier}>
+                  {lang.name}
+                </option>
+              ))}
+            </select>
+          )}
+          <button
+            className="py-2 px-4 mx-4 my-2 bg-purple-800 text-white rounded-lg"
+            onClick={handleGptSearchClick}
+          >
+            {showGptSearch ? "Homepage" : "GPT Search"}
           </button>
           <img
-            className="w-10 h-10 rounded"
-            alt="User avatar"
-            src="https://occ-0-2232-2186.1.nflxso.net/dnm/api/v6/0RO1pLmU93-gdXvuxd_iYjzPqkc/AAAAFGFzGNrVVKUbIWxDN8LFN3A1uPUoF9U4k0C6yzfq7IEiSB1wCeWdPyUWFnV_7y_aSWMJaXqW0l_Dn2gXxuVpS2X8Q6g0H.png?r=a41"
+            className="hidden md:block w-12 h-12"
+            alt="usericon"
+            src={user?.photoURL}
           />
-          <button
-            onClick={handleSignOut}
-            className="py-2 px-4 bg-red-700 text-white rounded-lg hover:bg-red-800"
-          >
-            Sign Out
+          <button onClick={handleSignOut} className="font-bold text-white ">
+            (Sign Out)
           </button>
         </div>
       )}
     </div>
   );
 };
-
 export default Header;
