2eb5a560931544600ae51c8a0be79b59f2b6e96b
sreeram780 <sreenu.ram2@gmail.com>
2026-06-08 11:27:33 +0530

    - updated main container component

--- src/Components/MainContainer.js
+++ src/Components/MainContainer.js
@@ -1,17 +1,21 @@
-import {useSelector} from "react-redux"
-import VideoBackGround from "./Videobackground";
-import { VideoTitle } from "./VideoTitle";
+import { useSelector } from "react-redux";
+import VideoBackground from "./VideoBackground";
+import VideoTitle from "./VideoTitle";
 
 const MainContainer = () => {
-    const movies = useSelector(store => store.movies?.nowPlayingMovies)
-    if (movies === null) return;
-    const mainMovie = movies[0];
-    console.log(mainMovie)
-    const {original_title, overview, id} = mainMovie
-    return <div>
-        <VideoTitle title={original_title} overview= {overview} />
-        <VideoBackGround id={id}/>
-    </div>;
-}
+  const movies = useSelector((store) => store.movies?.nowPlayingMovies);
 
+  if (!movies) return;
+
+  const mainMovie = movies[0];
+
+  const { original_title, overview, id } = mainMovie;
+
+  return (
+    <div className="pt-[30%] bg-black md:pt-0">
+      <VideoTitle title={original_title} overview={overview} />
+      <VideoBackground movieId={id} />
+    </div>
+  );
+};
 export default MainContainer;
