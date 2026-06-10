24f7cf0c6fd07b3586dc33d1c6aa1e1e56701d7d
sreeram780 <sreenu.ram2@gmail.com>
2026-06-10 11:53:21 +0530

    - updated moviecard component

--- src/Components/MovieCard.js
+++ src/Components/MovieCard.js
@@ -1,14 +1,11 @@
-import React from 'react'
-import { IMG_CDN_URL } from '../utils/constant'
+import { IMG_CDN_URL } from "../utils/constants";
 
-const MovieCard = () => {
+const MovieCard = ({ posterPath }) => {
+  if (!posterPath) return null;
   return (
-    <div>
-        <img alt="poster"
-        src = { IMG_CDN_URL + "" }
-        />
+    <div className="w-36 md:w-48 pr-4">
+      <img alt="Movie Card" src={IMG_CDN_URL + posterPath} />
     </div>
-  )
-}
-
-export default MovieCard
+  );
+};
+export default MovieCard;
